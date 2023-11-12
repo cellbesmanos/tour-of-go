8a2800ffc76bcb5c0e85260970f0fbdc167800cf
cellbesmanos <cell.besmanos0119@gmail.com>
2023-11-12 16:54:55 +0800

    Functions

--- main.go
+++ main.go
@@ -7,6 +7,26 @@ import (
 	"math/rand"
 )
 
+// this is a naked return, notice the return type has variables
+// this will automatically finds the variables within the function
+// and return their values
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	
+	// notice that removing this part will return y as 0 and not nil
+	y = sum - x
+	return
+}
+
+// reminds me of Python
+func swapValues(x, y string) (string, string) {
+	return y, x
+}
+
+func add(num1 int, num2 int) int {
+	return num1 + num2
+}
+
 func main() {
 	fmt.Println("Hello World!")
 
@@ -16,4 +36,14 @@ func main() {
 
 	// this is a custom package
 	greetings.Hello("test")
+
+	fmt.Println("\n", add(1, 2))
+
+	a := "World"
+	b := "Hello"
+	a, b = swapValues(a, b)
+
+	fmt.Println(a, b, "!")	
+
+	fmt.Println(split(17))
 }
