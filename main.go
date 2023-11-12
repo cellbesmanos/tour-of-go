675af6027af16c23f7b8d2748c00e6fc0ad35949
cellbesmanos <cell.besmanos0119@gmail.com>
2023-11-12 16:38:14 +0800

    Importing packages

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
 
 func main() {
 	fmt.Println("Hello World!")
+
+	fmt.Println("My favorite number is", rand.Intn(10))
+
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
 }
